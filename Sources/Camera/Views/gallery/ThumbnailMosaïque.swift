aab54f2c3762fc3915d1a8ab21cedb13f2eff20a
beateur <habilel99@gmail.com>
2022-05-12 17:11:23 +0200

    ___

---
+++ Sources/Camera/Views/gallery/ThumbnailMosaïque.swift
@@ -0,0 +1,64 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Bilel Hattay on 12/05/2022.
+//
+
+import SwiftUI
+
+struct ThumbnailMosaïqu: View {
+    @EnvironmentObject var galleryViewModel: ImagePickerViewModel
+
+    let gridItem = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    var body: some View {
+        VStack {
+            if galleryViewModel.libraryStatus != .authorized {
+                VStack(spacing: 8) {
+                    Text(galleryViewModel.libraryStatus == .denied ? "Acceder à votre gallerie" : "Plus de photos")
+                        .font(.system(size: 13))
+                        .foregroundColor(.gray)
+                    Button {
+                        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+                    } label: {
+                        Text(galleryViewModel.libraryStatus == .denied ? "Autoriser l'accès": "Sélectionner")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .padding()
+                            .background(Color.gray.opacity(0.9))
+                            .cornerRadius(12)
+                    }
+
+                }
+                .padding()
+            }
+
+            if galleryViewModel.fetchedElements.isEmpty {
+                emptyPlaceHolder
+            } else {
+                ScrollView {
+                    LazyVGrid(columns: GridItem, spacing: 0.5) {
+                        ForEach(galleryViewModel.fetchedElements, id: \.self) { Photo in
+                            ThumbnailView(photo: Photo)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private var emptyPlaceHolder: some View {
+        VStack {
+            Text("0 photo/vidéo")
+                .font(.title)
+            Text("Ajoutez des photos depuis votre gallerie")
+                .frame(width: UIScreen.main.bounds.width * 0.7)
+        }
+        .foregroundColor(Color.gray)
+    }
+}
