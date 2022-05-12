83dfe60628747f72315799d466849d851e44c51b
beateur <habilel99@gmail.com>
2022-05-12 18:05:29 +0200

    ___

--- Sources/Camera/ViewsModels/galleryViewModel.swift
+++ Sources/Camera/ViewsModels/galleryViewModel.swift
@@ -36,6 +36,7 @@ class ImagePickerViewModel: NSObject, ObservableObject {
     
     func openPickerMosaïque() {
         setup()
+
         withAnimation {
             showPickerMosaïque.toggle()
         }
