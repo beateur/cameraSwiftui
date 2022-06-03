d66e43cca53af28cef2d3d152f982f1a8379d139
beateur <habilel99@gmail.com>
2022-06-03 17:13:23 +0200

    ___

--- Sources/Camera/Views/gallery/ThumbnailList.swift
+++ Sources/Camera/Views/gallery/ThumbnailList.swift
@@ -20,6 +20,7 @@ struct ThumbList: View {
                 .resizable()
                 .font(.system(size: 20, weight: .bold))
                 .frame(width: 45, height: galleryViewModel.showPickerList ? 4 : 8)
+                .padding(.vertical, 8)
 
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 10) {
@@ -56,7 +57,7 @@ struct ThumbList: View {
             .frame(height: galleryViewModel.showPickerList ? UIScreen.main.bounds.size.height / 4 : 0)
             .opacity(galleryViewModel.showPickerList ? 1:0)
         }
-        .background(Color.primary.opacity(galleryViewModel.showPickerList ? 0.04: 0.01))
+        .background(Color.primary.opacity(galleryViewModel.showPickerList ? 0.08: 0.01))
         .gesture(
             DragGesture()
                 .onChanged { value in
