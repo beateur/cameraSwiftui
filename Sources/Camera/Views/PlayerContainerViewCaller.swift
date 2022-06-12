c75c1a6d112803253b7be650d5267b97ac63aad0
beateur <habilel99@gmail.com>
2022-06-12 16:23:34 +0200

    ___

--- Sources/Camera/Views/PlayerContainerViewCaller.swift
+++ Sources/Camera/Views/PlayerContainerViewCaller.swift
@@ -31,11 +31,8 @@ public struct PlayerContainerViewCaller: View {
             onUpdate()
         }
         .onReceive(Timer.publish(every: 0.1, on: .current, in: .common).autoconnect()) { _ in
-            print("\r\r\(Date())")
-            print(player.currentItem)
             if let item = player.currentItem {
-                print("evolution of currentTime \(player.currentTime())")
-                if player.currentTime() >= item.duration {
+                if player.currentTime().seconds >= item.duration.seconds {
                     playerVM.pause(player: player)
                     onEnd(player)
                 }
