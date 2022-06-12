9dc3fa29e455939120d84caf4e4369ffb6fe8b01
beateur <habilel99@gmail.com>
2022-06-12 16:11:17 +0200

    ___

--- Sources/Camera/Views/PlayerContainerViewCaller.swift
+++ Sources/Camera/Views/PlayerContainerViewCaller.swift
@@ -12,27 +12,29 @@ public struct PlayerContainerViewCaller: View {
     let player: AVPlayer
     let gravity: PlayerGravity
     
-    var onEditing: (AVPlayer)->()
-    public init(asset: AVAsset, gravity: PlayerGravity, onedition: @escaping(AVPlayer)->()) {
+    var onUpdate: ()->()
+    var onEnd: (AVPlayer)->()
+    public init(asset: AVAsset, gravity: PlayerGravity, onedition: @escaping()->(), onEnd: @escaping(AVPlayer)->()) {
         self.gravity = gravity
         
         let playerItem = AVPlayerItem(asset: asset)
         let player = AVPlayer(playerItem: playerItem)
         
         self.player = player
-        self.onEditing = onedition
+        self.onUpdate = onedition
+        self.onEnd = onEnd
     }
     
     public var body: some View {
         let playerVM = PlayerViewModel.shared
         PlayerContainerView(player: player, gravity: gravity) {
-            onEditing(player)
+            onUpdate()
         }
         .onReceive(Timer.publish(every: 0.1, on: .current, in: .common).autoconnect()) { _ in
             if let item = player.currentItem {
                 if player.currentTime() >= item.duration {
                     playerVM.pause(player: player)
-                    onEditing(player)
+                    onEnd(player)
                 }
             }
         }
