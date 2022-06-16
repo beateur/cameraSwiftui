b952c5cd7e78a5836ccbe84eeacd04f8102a9fcc
beateur <habilel99@gmail.com>
2022-06-16 12:34:21 +0200

    ___

--- Sources/Camera/ViewsModels/PlayerViewModel.swift
+++ Sources/Camera/ViewsModels/PlayerViewModel.swift
@@ -10,10 +10,11 @@ import AVFoundation
 
 public class PlayerViewModel: ObservableObject {
     public static let shared = PlayerViewModel()
-    private var observer: NSObjectProtocol?
     
     public func play(player: AVPlayer) {
         let currentItem = player.currentItem
+        
+        print("launched video: \rtime: \(currentItem?.currentTime())\rduration: \(currentItem?.duration)")
         if currentItem?.currentTime() == currentItem?.duration {
             currentItem?.seek(to: .zero, completionHandler: nil)
         }
@@ -21,15 +22,17 @@ public class PlayerViewModel: ObservableObject {
     }
     
     public func loopVideo(videoPlayer: AVPlayer) {
-        observer = NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil, queue: nil) { _ in
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil, queue: nil) { _ in
             self.play(player: videoPlayer)
         }
     }
     
     public func pause(player: AVPlayer) {
         player.pause()
-        if let _ = observer {
-            NotificationCenter.default.removeObserver(observer!)
-        }
+    }
+    
+    public func stop(player: AVPlayer) {
+        player.pause()
+        player.seek(to: CMTime.init(seconds: 0, preferredTimescale: 1))
     }
 }
