83874a165986c26ec24e5f6a6b1a0696a8f2537d
beateur <habilel99@gmail.com>
2023-02-02 02:53:05 +0100

    debug gallery picker

--- Sources/Camera/ViewsModels/InstanceViewModel.swift
+++ Sources/Camera/ViewsModels/InstanceViewModel.swift
@@ -13,7 +13,7 @@ import AVFoundation
 class cameraInstanceViewModel: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate, AVCaptureFileOutputRecordingDelegate {
     static let shared = cameraInstanceViewModel()
 
-    @Published var session = AVCaptureSession()
+    let session = AVCaptureSession()
     @Published var alert = false
     @Published var photoOutput = AVCapturePhotoOutput()
     @Published var movieOutput = AVCaptureMovieFileOutput()
